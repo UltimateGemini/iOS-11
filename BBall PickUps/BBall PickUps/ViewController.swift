e35f9b69db0179ec0d0026273074f8e55dfe24bb
Rotor7 <Rotor7@users.noreply.github.com>
2017-08-23 20:54:28 -0400

    Stack views
    Added stackviews ti rest of the screens and added back button functionality.

--- BBall PickUps/BBall PickUps/ViewController.swift
+++ BBall PickUps/BBall PickUps/ViewController.swift
@@ -21,11 +21,8 @@ class ViewController: UIViewController {
         
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func unwindFromSkillVC (unwindSeague: UIStoryboardSegue) {
+        
     }
-
-
 }
 

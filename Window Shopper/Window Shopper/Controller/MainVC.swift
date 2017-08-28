2ad270be69f795d633096059f6bb80d2e4eee414
Rotor7 <Rotor7@users.noreply.github.com>
2017-08-28 12:52:09 -0400

    Custom text fields
    Custom text fields

---
+++ Window Shopper/Window Shopper/Controller/MainVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Window Shopper
+//
+//  Created by Noel Franceschi on 8/28/17.
+//  Copyright © 2017 Noel Franceschi. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Window Shopper/Window Shopper/Views/CurrencyTxtField.swift
@@ -0,0 +1,27 @@
+//
+//  CurrencyTxtField.swift
+//  Window Shopper
+//
+//  Created by Noel Franceschi on 8/28/17.
+//  Copyright © 2017 Noel Franceschi. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTxtField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2548415493)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+        
+        
+    }
+}

7efa57618c61e704ccb79fdca8eb59fc009e8885
Rotor7 <Rotor7@users.noreply.github.com>
2017-08-28 14:49:42 -0400

    Added custom currency label
    Created custom label in code and added its position and currency type.

--- Window Shopper/Window Shopper/Views/CurrencyTxtField.swift
+++ Window Shopper/Window Shopper/Views/CurrencyTxtField.swift
@@ -12,6 +12,26 @@ import UIKit
 @IBDesignable
 class CurrencyTxtField: UITextField {
 
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        
+        let currencyLbl = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2) - size / 2, width: size, height: size))
+        currencyLbl.backgroundColor = #colorLiteral(red: 0.9049578497, green: 0.9049578497, blue: 0.9049578497, alpha: 0.8005336708)
+        currencyLbl.textAlignment = .center
+        currencyLbl.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        currencyLbl.layer.cornerRadius = 5.0
+        currencyLbl.clipsToBounds = true
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        
+        currencyLbl.text = formatter.currencySymbol
+        
+        addSubview(currencyLbl)
+        
+    }
+    
     override func prepareForInterfaceBuilder() {
         customizeView()
     }
@@ -25,6 +45,8 @@ class CurrencyTxtField: UITextField {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2548415493)
         layer.cornerRadius = 5.0
         textAlignment = .center
+        
+        clipsToBounds = true
         
         if let p = placeholder {
             let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])

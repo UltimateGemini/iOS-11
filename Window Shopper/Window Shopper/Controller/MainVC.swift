2bae9babba8650c55c267927280cdab16a87bd44
Rotor7 <Rotor7@users.noreply.github.com>
2017-08-28 13:27:39 -0400

    Input accessory views and IBdesignable
    - Added a button in code to main View so that when keyboard opens, the button will show above keyboard.
    
    - Added IBDesignable to top of custom class so the interface builder will look the same as the code

--- Window Shopper/Window Shopper/Controller/MainVC.swift
+++ Window Shopper/Window Shopper/Controller/MainVC.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class MainVC: UIViewController {
 
+    @IBOutlet weak var wageTxt: CurrencyTxtField!
+    @IBOutlet weak var priceTxt: CurrencyTxtField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        calcBtn.backgroundColor = #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)
+        calcBtn.setTitle("Calculate", for: .normal)
+        calcBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: UIControlState.normal)
+        calcBtn.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        
+        wageTxt.inputAccessoryView = calcBtn
+        priceTxt.inputAccessoryView = calcBtn
     }
-
+    
+    @objc func calculate() {
+        
+    }
 
 }
 
--- Window Shopper/Window Shopper/Views/CurrencyTxtField.swift
+++ Window Shopper/Window Shopper/Views/CurrencyTxtField.swift
@@ -8,10 +8,20 @@
 
 import UIKit
 
+
+@IBDesignable
 class CurrencyTxtField: UITextField {
 
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        customizeView()
+    }
+    
+    func customizeView() {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2548415493)
         layer.cornerRadius = 5.0
         textAlignment = .center
@@ -21,7 +31,5 @@ class CurrencyTxtField: UITextField {
             attributedPlaceholder = place
             textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         }
-        
-        
     }
 }

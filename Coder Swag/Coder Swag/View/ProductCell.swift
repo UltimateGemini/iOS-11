c1acc9b5127d9b017283166c04c4a21eb6dcb2fc
Rotor7 <Rotor7@users.noreply.github.com>
2017-08-30 11:40:14 -0400

    Added Collection View
    Added products screen with collection view and connected outlets.

---
+++ Coder Swag/Coder Swag/View/ProductCell.swift
@@ -0,0 +1,18 @@
+//
+//  ProductCell.swift
+//  Coder Swag
+//
+//  Created by Noel Franceschi on 8/30/17.
+//  Copyright © 2017 Noel Franceschi. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+    
+}

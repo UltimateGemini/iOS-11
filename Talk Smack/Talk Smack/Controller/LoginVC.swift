2f479035afac27bae244804f138ffbc108ef94a5
Rotor7 <Rotor7@users.noreply.github.com>
2017-08-31 15:16:48 -0400

    Created UI for Login VC
    - Connected UI elements to storyboard
    
    - Created a couple of VCs

---
+++ Talk Smack/Talk Smack/Controller/LoginVC.swift
@@ -0,0 +1,30 @@
+//
+//  LoginVCV.swift
+//  Talk Smack
+//
+//  Created by Noel Franceschi on 8/31/17.
+//  Copyright © 2017 Noel Franceschi. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    //: Oulets
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func closePressed(_ sender: Any) {
+        
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ Talk Smack/Talk Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Talk Smack
+//
+//  Created by Noel Franceschi on 8/31/17.
+//  Copyright © 2017 Noel Franceschi. All rights reserved.
+//
+
+import Foundation
+
+//: Segues
+let TO_LOGIN = "toLogin"

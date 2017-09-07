1367e4b3ccef75156583386198d039496edb3926
Rotor7 <Rotor7@users.noreply.github.com>
2017-09-07 17:55:33 -0400

    Created web request in Xcode
    - Connected Heroku Cloud DB
    - Setting up registering a user

---
+++ Talk Smack/Talk Smack/Services/AuthService.swift
@@ -0,0 +1,95 @@
+//
+//  AuthService.swift
+//  Talk Smack
+//
+//  Created by Noel Franceschi on 9/7/17.
+//  Copyright © 2017 Noel Franceschi. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class AuthService {
+    
+    static let instance = AuthService()
+    
+    let defaults = UserDefaults.standard
+    
+    var isLoggedIn : Bool {
+        
+        get{
+            
+            return defaults.bool(forKey: LOGGED_IN_KEY)
+        }
+        
+        set {
+            
+            defaults.set(newValue, forKey: LOGGED_IN_KEY)
+        }
+    }
+    
+    var authToke: String {
+        
+        get {
+            
+            return defaults.value(forKey: TOKEN_KEY) as! String
+        }
+        
+        set {
+            
+            defaults.set(newValue, forKey: TOKEN_KEY)
+        }
+    }
+    
+    var userEmail: String {
+        
+        get {
+            
+            return defaults.value(forKey: USER_EMAIL) as! String
+        }
+        
+        set {
+            
+            defaults.set(newValue, forKey: USER_EMAIL)
+        }
+    }
+    
+    func registerUser(email: String, password: String, completion: @escaping CompletionHandler) {
+        
+        let lowerCaseEmail = email.lowercased()
+        
+        let header = [
+        
+            "Content-Type" : "application/json; charset=utf-8"
+        ]
+        
+        let body: [String: Any] = [
+            
+            "email": lowerCaseEmail,
+            "password": password
+        ]
+        
+        Alamofire.request(URL_REGISTER,
+                          method: .post,
+                          parameters: body,
+                          encoding: JSONEncoding.default,
+                          headers: header).responseString
+            { (response) in
+                
+                if response.result.error == nil {
+                    completion(true)
+                } else {
+                    completion(false)
+                    debugPrint(response.result.error as Any)
+                }
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+}
--- Talk Smack/Talk Smack/Utilities/Constants.swift
+++ Talk Smack/Talk Smack/Utilities/Constants.swift
@@ -8,7 +8,21 @@
 
 import Foundation
 
+typealias CompletionHandler = ( Success: Bool) -> ()
+
+//: URL Constants
+let BASE_UR = "https://talksmack.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
 //: Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+
+//: User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
+

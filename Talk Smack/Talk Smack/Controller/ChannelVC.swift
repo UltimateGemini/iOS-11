ca661be8201c15c85671af107f9544e61b514a93
Rotor7 <Rotor7@users.noreply.github.com>
2017-09-10 17:26:38 -0400

    Completed LoggedIn Interface
    - Added Observer that listens to created user
    - added scanner for working with strings
    - added background color to logged in avatar

--- Talk Smack/Talk Smack/Controller/ChannelVC.swift
+++ Talk Smack/Talk Smack/Controller/ChannelVC.swift
@@ -11,6 +11,7 @@ import UIKit
 class ChannelVC: UIViewController {
 
     //: Outlets
+    @IBOutlet weak var userImg: CircleImage!
     @IBOutlet weak var loginBtn: UIButton!
     @IBAction func prepareForUnwind(seague: UIStoryboardSegue) {}
     
@@ -18,10 +19,30 @@ class ChannelVC: UIViewController {
         super.viewDidLoad()
 
         self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(ChannelVC.userDataDidChange(notif:)), name: NOTI_USER_DATA_DID_CHANGE, object: nil)
     }
 
     @IBAction func loginBtnPressed(_ sender: Any) {
         
         performSegue(withIdentifier: TO_LOGIN, sender: nil)
     }
+
+    @objc func userDataDidChange( notif: Notification) {
+        
+        if AuthService.instance.isLoggedIn {
+            
+            loginBtn.setTitle(UserDataService.instance.name, for: .normal)
+            userImg.image = UIImage(named: UserDataService.instance.avatarName)
+            userImg.backgroundColor = UserDataService.instance.returnUIColor(components: UserDataService.instance.avatarColor)
+            
+        } else {
+            
+            loginBtn.setTitle("Login", for: .normal)
+            userImg.image = UIImage(named: "menuProfileIcon")
+            userImg.backgroundColor = UIColor.clear
+            
+        }
+    }
 }
+
--- Talk Smack/Talk Smack/Controller/CreateAccountVC.swift
+++ Talk Smack/Talk Smack/Controller/CreateAccountVC.swift
@@ -90,6 +90,7 @@ class CreateAccountVC: UIViewController {
         let b = CGFloat(arc4random_uniform(255)) / 255
         
         bgColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+        avatarColor = "[\(r), \(g), \(b), 1]"
         UIView.animate(withDuration: 0.2) {
             self.userImg.backgroundColor = self.bgColor
         }

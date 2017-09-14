d4242c9c1e683670392644703d2711905827ee44
Rotor7 <Rotor7@users.noreply.github.com>
2017-09-14 19:26:03 -0400

    Completed Sending First Message
    -  Implemented sockets to send message and retrieve it.

--- Talk Smack/Talk Smack/Services/MessageService.swift
+++ Talk Smack/Talk Smack/Services/MessageService.swift
@@ -52,6 +52,7 @@ class MessageService {
     func findAllMessagesForChannel(channelId: String, completion: @escaping CompletionHandler) {
         
         Alamofire.request("\(URL_GET_MESSAGES)\(channelId)", method: .get, parameters: nil, encoding: JSONEncoding.default, headers: BEARER_HEADER).responseJSON { (response) in
+            
             if response.result.error == nil {
                 
                 self.clearMessages()
--- Talk Smack/Talk Smack/Services/SocketService.swift
+++ Talk Smack/Talk Smack/Services/SocketService.swift
@@ -11,44 +11,46 @@ import SocketIO
 
 class SocketService: NSObject {
 
-        static let instance = SocketService()
+    static let instance = SocketService()
     
-        override init() {
-            super.init()
+    override init() {
+        super.init()
     
-        }
+    }
     
-        var socket : SocketIOClient = SocketIOClient(socketURL: URL(string: BASE_URL)!)
+    var socket : SocketIOClient = SocketIOClient(socketURL: URL(string: BASE_URL)!)
     
-        func establishConnection() {
+    func establishConnection() {
     
-            socket.connect()
-        }
+        socket.connect()
+    }
     
-        func closeConnection() {
+    func closeConnection() {
     
-            socket.disconnect()
-        }
+        socket.disconnect()
+    }
     
-        func addChannel(channelName: String, channelDescription: String, completion: @escaping CompletionHandler) {
+    func addChannel(channelName: String, channelDescription: String, completion: @escaping CompletionHandler) {
     
-            socket.emit("newChannel", channelName, channelDescription)
+        socket.emit("newChannel", channelName, channelDescription)
+        completion(true)
+    }
+    
+    func getChannel(completion: @escaping CompletionHandler) {
+        socket.on("channelCreated") { (dataArray, ack) in
+            guard let channelName = dataArray[0] as? String else { return }
+            guard let channelDesc = dataArray[1] as? String else { return }
+            guard let channelId = dataArray[2] as? String else { return }
+            
+            let newChannel = Channel(channelTitle: channelName, channelDescription: channelDesc, id: channelId)
+            MessageService.instance.channels.append(newChannel)
             completion(true)
-    
-    
         }
+    }
     
-        func getChannel(completion: @escaping CompletionHandler) {
-    
-            socket.on("channelCreated") { (dataArray,ack) in
-    
-                guard let channelName = dataArray[0] as? String else { return }
-                guard let channelDesc = dataArray[1] as? String else { return }
-                guard let channelId = dataArray[2] as? String else { return }
-    
-                let newChannel = Channel(channelTitle: channelName, channelDescription: channelDesc, id: channelId)
-                MessageService.instance.channels.append(newChannel)
-                completion(true)
-            }
-        }
+    func addMessage(messageBody: String, userId: String, channelId: String, completion: @escaping CompletionHandler) {
+        let user = UserDataService.instance
+        socket.emit("newMessage", messageBody, userId, channelId, user.name, user.avatarName, user.avatarColor)
+        completion(true)
+    }
 }

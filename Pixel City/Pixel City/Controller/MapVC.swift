b8adedbe3f097660cf9d2eec7cae5635e5eaf1a5
Rotor7 <Rotor7@users.noreply.github.com>
2017-09-14 22:48:29 -0400

    Created Project & Installed CocoaPods
    - Installed Alamofire
    - Installed AlamofireImage

---
+++ Pixel City/Pixel City/Controller/MapVC.swift
@@ -0,0 +1,25 @@
+//
+//  MapVC.swift
+//  Pixel City
+//
+//  Created by Noel Franceschi on 9/14/17.
+//  Copyright © 2017 Noel Franceschi. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

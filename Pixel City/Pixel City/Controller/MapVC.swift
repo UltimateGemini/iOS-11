b967ecf4894ae2f326feb27d71a748fa74db6625
Rotor7 <Rotor7@users.noreply.github.com>
2017-09-15 10:51:25 -0400

    Completed Requesting Location Services
    - Set up location services
    - Added option to allow or not use of location
    - Centering Map on user location

--- Pixel City/Pixel City/Controller/MapVC.swift
+++ Pixel City/Pixel City/Controller/MapVC.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import MapKit
+import CoreLocation
+
 
 class MapVC: UIViewController {
 
@@ -15,23 +17,45 @@ class MapVC: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
     
     
-    
-    
+    //: Vars
+    var locationManager = CLLocationManager()
+    let authStatus = CLLocationManager.authorizationStatus()
+    let regionRadius: Double = 1000
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         mapView.delegate = self
+        locationManager.delegate = self
+        configureLocationServices()
     }
     
     @IBAction func centerMapBtnWasPressed(_ sender: Any) {
         
+        if authStatus == .authorizedAlways || authStatus == .authorizedWhenInUse {
+            centerMapOnUserLocation()
+        }
     }
     
-
 }
 
 extension MapVC: MKMapViewDelegate {
+    func centerMapOnUserLocation() {
+        guard let coordinate = locationManager.location?.coordinate else { return }
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(coordinate, regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+}
+
+extension MapVC: CLLocationManagerDelegate {
+    func configureLocationServices() {
+        if authStatus == .notDetermined {
+            locationManager.requestAlwaysAuthorization()
+        } else {
+            return
+        }
+    }
     
-    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        centerMapOnUserLocation()
+    }
 }
 

36357eb49d47343e8978a7443ad81c3a78034ad3
Rotor7 <Rotor7@users.noreply.github.com>
2017-09-18 13:47:02 -0400

    Complete App 100%
    - Completed handling purchase
    - Completed restoring purchase

--- Daily Dose/Daily Dose/Controller/HomeVC.swift
+++ Daily Dose/Daily Dose/Controller/HomeVC.swift
@@ -14,18 +14,51 @@ class HomeVC: UIViewController {
 
     //: Outlets
     @IBOutlet weak var bannerView: GADBannerView!
-    
+    @IBOutlet weak var removeAdsBtn: UIButton!
     
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupAds()
         
-        bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
-        bannerView.rootViewController = self
-        bannerView.load(GADRequest())
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func setupAds() {
+        if UserDefaults.standard.bool(forKey: PurchaseManager.instance.IAP_REMOVE_ADS) {
+            removeAdsBtn.removeFromSuperview()
+            bannerView.removeFromSuperview()
+        } else {
+            bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
+            bannerView.rootViewController = self
+            bannerView.load(GADRequest())
+        }
     }
 
+    @IBAction func restoreBtnPressed(_ sender: Any) {
+        PurchaseManager.instance.restorePurchases { success in
+            if success {
+                self.setupAds()
+            }
+        }
+    }
+
+    @IBAction func removeAdsPressed(_ sender: Any) {
+        PurchaseManager.instance.purchaseRemoveAds { success in
+            if success {
+                self.bannerView.removeFromSuperview()
+                self.removeAdsBtn.removeFromSuperview()
+            } else {
+                //show message to the user
+            }
+        }
+    }
 }
 
--- Daily Dose/Daily Dose/Services/PurchaseManager.swift
+++ Daily Dose/Daily Dose/Services/PurchaseManager.swift
@@ -6,27 +6,49 @@
 //  Copyright © 2017 Noel Franceschi. All rights reserved.
 //
 
+typealias CompletionHandler = (_ success: Bool) -> ()
+
 import Foundation
 import StoreKit
 
 class PurchaseManager: NSObject, SKProductsRequestDelegate, SKPaymentTransactionObserver {
-    
     static let instance = PurchaseManager()
     
-    let IAP_REMOVE_ADS = "com.noelfranceschi.daily.dose.remove.ads"
+    let IAP_REMOVE_ADS = "com.devslopes.daily.dose.remove.ads"
     
-    //: Vars
-    var productRequest: SKProductsRequest!
+    var productsRequest: SKProductsRequest!
     var products = [SKProduct]()
+    var transactionComplete: CompletionHandler?
     
     func fetchProducts() {
-        
         let productIds = NSSet(object: IAP_REMOVE_ADS) as! Set<String>
-        productRequest = SKProductsRequest(productIdentifiers: productIds)
-        productRequest.delegate = self
-        productRequest.start()
+        productsRequest = SKProductsRequest(productIdentifiers: productIds)
+        productsRequest.delegate = self
+        productsRequest.start()
     }
     
+    func purchaseRemoveAds(onComplete: @escaping CompletionHandler) {
+        if SKPaymentQueue.canMakePayments() && products.count > 0 {
+            transactionComplete = onComplete
+            let removeAdsProduct = products[0]
+            let payment = SKPayment(product: removeAdsProduct)
+            SKPaymentQueue.default().add(self)
+            SKPaymentQueue.default().add(payment)
+        } else {
+            onComplete(false)
+        }
+    }
+    
+    func restorePurchases(onComplete: @escaping CompletionHandler) {
+        if SKPaymentQueue.canMakePayments() {
+            transactionComplete = onComplete
+            SKPaymentQueue.default().add(self)
+            SKPaymentQueue.default().restoreCompletedTransactions()
+        } else {
+            onComplete(false)
+        }
+        
+    }
     
     func productsRequest(_ request: SKProductsRequest, didReceive response: SKProductsResponse) {
         
@@ -38,9 +60,30 @@ class PurchaseManager: NSObject, SKProductsRequestDelegate, SKPaymentTransaction
     }
     
     func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
-        
-        
+        for transaction in transactions {
+            switch transaction.transactionState {
+            case .purchased:
+                SKPaymentQueue.default().finishTransaction(transaction)
+                if transaction.payment.productIdentifier == IAP_REMOVE_ADS {
+                    UserDefaults.standard.set(true, forKey: IAP_REMOVE_ADS)
+                    transactionComplete?(true)
+                }
+                break
+            case .failed:
+                SKPaymentQueue.default().finishTransaction(transaction)
+                transactionComplete?(false)
+                break
+            case .restored:
+                SKPaymentQueue.default().finishTransaction(transaction)
+                if transaction.payment.productIdentifier == IAP_REMOVE_ADS {
+                    UserDefaults.standard.set(true, forKey: IAP_REMOVE_ADS)
+                }
+                transactionComplete?(true)
+            default:
+                transactionComplete?(false)
+                break
+            }
+        }
     }
-    
     
 }

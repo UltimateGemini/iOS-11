dd600b21903213596cecab38c172f03e72bf7fab
Rotor7 <Rotor7@users.noreply.github.com>
2017-09-18 14:11:26 -0400

    Created Project
    - Building GoalsVC

---
+++ GoalPost/GoalPost/Controller/GoalsVC.swift
@@ -0,0 +1,35 @@
+//
+//  GoalsVC.swift
+//  GoalPost
+//
+//  Created by Noel Franceschi on 9/18/17.
+//  Copyright © 2017 Noel Franceschi. All rights reserved.
+//
+
+import UIKit
+
+class GoalsVC: UIViewController {
+
+    
+    //: Outlets
+    @IBOutlet weak var tabelView: UITableView!
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    
+    @IBAction func addGoalBtnWasPressed(_ sender: Any) {
+        
+    }
+    
+
+}
+

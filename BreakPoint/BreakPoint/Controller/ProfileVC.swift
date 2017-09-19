b381535f4201cd5ceedcafb35ec57e6aa4403127
Rotor7 <Rotor7@users.noreply.github.com>
2017-09-19 13:50:57 -0400

    Created ProfileVC
    - Created interface and added to tab bar.

---
+++ BreakPoint/BreakPoint/Controller/ProfileVC.swift
@@ -0,0 +1,32 @@
+//
+//  ProfileVC.swift
+//  BreakPoint
+//
+//  Created by Noel Franceschi on 9/19/17.
+//  Copyright © 2017 Noel Franceschi. All rights reserved.
+//
+
+import UIKit
+
+class ProfileVC: UIViewController {
+
+    
+    //: Outlets
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var tabelView: UITableView!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    @IBAction func signOutBtnWasPressed(_ sender: Any) {
+        
+    }
+
+}

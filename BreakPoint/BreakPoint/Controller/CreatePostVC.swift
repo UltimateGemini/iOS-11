acc6fe8cff65aa2ed7a8585b4960f73b13d73632
Rotor7 <Rotor7@users.noreply.github.com>
2017-09-19 14:25:24 -0400

    Created CreatePostVC
    - Created new post interface
    - Connect to Firebase tin order to add post to db.

---
+++ BreakPoint/BreakPoint/Controller/CreatePostVC.swift
@@ -0,0 +1,52 @@
+//
+//  CreatePostVC.swift
+//  BreakPoint
+//
+//  Created by Noel Franceschi on 9/19/17.
+//  Copyright © 2017 Noel Franceschi. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+
+class CreatePostVC: UIViewController {
+
+    
+    //: Outlets
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var sendBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        textView.delegate = self
+    }
+
+    @IBAction func sendBtnWasPressed(_ sender: Any) {
+        if textView.text != nil && textView.text != "Write message here..." {
+            sendBtn.isEnabled = false
+            DataService.instance.uploadPost(withMessage: textView.text, forUID: (Auth.auth().currentUser?.uid)!, withGroupKey: nil, sendComplete: { (isComplete) in
+                if isComplete {
+                    self.sendBtn.isEnabled = true
+                    self.dismiss(animated: true, completion: nil)
+                } else {
+                    self.sendBtn.isEnabled = true
+                    print("There was an error!")
+                }
+            })
+        }
+    }
+    
+    @IBAction func closeBtnWasPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
+
+extension CreatePostVC: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textView.text = ""
+    }
+}
--- BreakPoint/BreakPoint/Services/DataService.swift
+++ BreakPoint/BreakPoint/Services/DataService.swift
@@ -40,6 +40,15 @@ class DataService {
         REF_USERS.child(uid).updateChildValues(userData)
     }
     
+    func uploadPost(withMessage message: String, forUID uid: String, withGroupKey groupKey: String?, sendComplete: @escaping (_ status: Bool) -> ()) {
+        if groupKey != nil {
+            
+        } else {
+            _REF_FEED.childByAutoId().updateChildValues(["content": message, "senderId": uid])
+            sendComplete(true)
+        }
+    }
+    
 }
 
 

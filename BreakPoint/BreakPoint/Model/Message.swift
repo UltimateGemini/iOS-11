fc5d02e9e784ece3308b7089120a109533c46eb3
Rotor7 <Rotor7@users.noreply.github.com>
2017-09-19 15:20:06 -0400

    Completed Feed Model
    - Created Message model
    - Created function to download all of feed messages from firebase and display in tableview.

---
+++ BreakPoint/BreakPoint/Model/Message.swift
@@ -0,0 +1,27 @@
+//
+//  Message.swift
+//  BreakPoint
+//
+//  Created by Noel Franceschi on 9/19/17.
+//  Copyright © 2017 Noel Franceschi. All rights reserved.
+//
+
+import Foundation
+
+class Message {
+    private var _content: String
+    private var _senderId: String
+    
+    var content: String {
+        return _content
+    }
+    
+    var senderId: String {
+        return _senderId
+    }
+    
+    init(content: String, senderId: String) {
+        self._content = content
+        self._senderId = senderId
+    }
+}
--- BreakPoint/BreakPoint/Services/DataService.swift
+++ BreakPoint/BreakPoint/Services/DataService.swift
@@ -49,6 +49,23 @@ class DataService {
         }
     }
     
+    func getAllFeedMessages(handler: @escaping(_ messages: [Message]) -> ()) {
+        
+        var messageArray = [Message]()
+        
+        REF_FEED.observeSingleEvent(of: .value) { (feedMessageSnapshot) in
+            guard let feedMessageSnapshot = feedMessageSnapshot.children.allObjects as? [DataSnapshot] else { return }
+            
+            for message in feedMessageSnapshot {
+                let content = message.childSnapshot(forPath: "content").value as! String
+                let senderId = message.childSnapshot(forPath: "senderId").value as! String
+                let message = Message(content: content, senderId: senderId)
+                messageArray.append(message)
+            }
+            
+            handler(messageArray)
+        }
+    }
 }
 
 

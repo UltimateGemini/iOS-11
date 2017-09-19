8ddf19a24e1d75e23d78a36b4d732ee837340a27
Rotor7 <Rotor7@users.noreply.github.com>
2017-09-19 14:34:06 -0400

    Completed binding keyboard
    - Created extention to bind sendbtn when keyboard sldies up

--- BreakPoint/BreakPoint/Controller/CreatePostVC.swift
+++ BreakPoint/BreakPoint/Controller/CreatePostVC.swift
@@ -22,6 +22,7 @@ class CreatePostVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         textView.delegate = self
+        sendBtn.bindToKeyboard()
     }
 
     @IBAction func sendBtnWasPressed(_ sender: Any) {

3b11438cdfb9a808295f4e97c9000fe5363addbb
Rotor7 <Rotor7@users.noreply.github.com>
2017-09-19 12:31:30 -0400

    Setting up AuthService
    - Created registerUser and loginUser with email functions.

---
+++ BreakPoint/BreakPoint/Services/AuthService.swift
@@ -0,0 +1,43 @@
+//
+//  AuthService.swift
+//  BreakPoint
+//
+//  Created by Noel Franceschi on 9/19/17.
+//  Copyright © 2017 Noel Franceschi. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+
+class AuthService {
+    static let instance = AuthService()
+    
+    func registerUser(withEmail email: String, andPassword password: String, userCreationComplete: @escaping (_ status: Bool, _ error: Error?) -> ()) {
+        
+        Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+            guard let user = user else {
+                userCreationComplete(false, error)
+                return
+            }
+            
+            let userData = ["provider": user.providerID, "email": user.email]
+            DataService.instance.createDBUser(uid: user.uid, userData: userData)
+            userCreationComplete(true, nil)
+        }
+    }
+    
+    func loginUser(withEmail email: String, andPassword password: String, loginComplete: @escaping (_ status: Bool, _ error: Error?) -> ()) {
+        
+        Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+            guard let user = user else {
+                loginComplete(false, error)
+                return
+            }
+            
+            loginComplete(true, nil)
+        }
+    }
+    
+    
+}

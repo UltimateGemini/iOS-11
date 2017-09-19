5f384827f3f559295b2356a72128cd028cfe0db3
Rotor7 <Rotor7@users.noreply.github.com>
2017-09-19 10:50:48 -0400

    Created project
    - Created projects files
    - Added assets

---
+++ BreakPoint/BreakPoint/Controller/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  BreakPoint
+//
+//  Created by Noel Franceschi on 9/19/17.
+//  Copyright © 2017 Noel Franceschi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

bf7545cd3fbec9264ffff917596603a6c9a4d691
Rotor7 <Rotor7@users.noreply.github.com>
2017-09-21 14:30:51 -0400

    Created Project

---
+++ Visions/Visions/Controller/CameraVC.swift
@@ -0,0 +1,19 @@
+//
+//  CameraVC.swift
+//  Visions
+//
+//  Created by Noel Franceschi on 9/21/17.
+//  Copyright © 2017 Noel Franceschi. All rights reserved.
+//
+
+import UIKit
+
+class CameraVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+

ec309525ed772cc1857300341792370d586bd766
Rotor7 <Rotor7@users.noreply.github.com>
2017-09-28 12:13:40 -0400

    Implemented 3D Model in popover
    - Created new ramps.scn

--- Ramp Park/Ramp Park/Controller/RampPickerVC.swift
+++ Ramp Park/Ramp Park/Controller/RampPickerVC.swift
@@ -32,7 +32,24 @@ class RampPickerVC: UIViewController {
         sceneView = SCNView(frame: CGRect(x: 0, y: 0, width: size.width, height: size.height))
         view.insertSubview(sceneView, at: 0)
         
+        let scene = SCNScene(named: "art.scnassets/ramps.scn")!
+        sceneView.scene = scene
+        
+        let camera = SCNCamera()
+        camera.usesOrthographicProjection = true
+        scene.rootNode.camera = camera
+        
+        let obj = SCNScene(named: "art.scnassets/pipe.dae")
+        let node = obj?.rootNode.childNode(withName: "pipe", recursively: true)!
+        
+        node?.scale = SCNVector3Make(0.0025, 0.0025, 0.0025)
+        node?.position = SCNVector3Make(-1, 0.7, -1)
+        
+        scene.rootNode.addChildNode(node!)
+        
         preferredContentSize = size
+        
+        
     }
 
     
